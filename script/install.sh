9fefe77848da9659a411e164a70ba52c4ae65f55
ParalaxRus <paralax_rus@hotmail.com>
2023-02-25 13:42:39 -0800

    install.sh fix

--- script/install.sh
+++ script/install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-cd ~
-
 apt -y update
 apt -y install git
 apt -y install g++
@@ -9,13 +7,17 @@ apt-get -y install libboost-all-dev
 apt-get -y install cmake
 
 git clone https://github.com/ParalaxRus/FileManager.git
-
 cd FileManager/
+
 mkdir build
 cd build/
-cmake . || exit 1
+cmake .. || exit 1
+
 cmake --build . || exit 1
+
 ctest || exit 1
+
 pkgvar=$(cpack | grep -Po 'FileManagerPrj.*deb' | xargs -L 1 basename)
 dpkg --install $pkgvar
-echo "filemanager should be installed into the home folder"
+
+echo "filemanager tool should be located in the 'home' folder"
