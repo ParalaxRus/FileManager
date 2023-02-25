fc3ee353238d948ddb8db87af2ca85e9665212eb
ParalaxRus <paralax_rus@hotmail.com>
2023-02-25 12:56:45 -0800

    install script added

---
+++ script/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd ~
+
+apt -y update
+apt -y install git
+apt -y install g++
+apt-get -y install libboost-all-dev
+apt-get -y install cmake
+
+git clone https://github.com/ParalaxRus/FileManager.git
+
+cd FileManager/
+mkdir build
+cd build/
+cmake . || exit 1
+cmake --build . || exit 1
+ctest || exit 1
+pkgvar=$(cpack | grep -Po 'FileManagerPrj.*deb' | xargs -L 1 basename)
+dpkg --install $pkgvar
+echo "filemanager should be installed into the home folder"
