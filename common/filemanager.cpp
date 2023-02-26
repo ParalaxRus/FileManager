08d449827db3c2735dcd33ec8d5b99ed2af6f30a
ParalaxRus <paralax_rus@hotmail.com>
2023-02-25 18:36:45 -0800

    retry added

--- common/filemanager.cpp
+++ common/filemanager.cpp
@@ -5,9 +5,43 @@
 
 namespace task {
 
+namespace {
+    const int kRetryCount = 3;
+    const int kRetryWaitMs = 5;
+}
+
 bool FileManager::exists(const std::string& src) {
     std::ifstream input(src);
     return input.good();
+}
+
+bool FileManager::createFile(const std::string& src, const std::string& text) {
+    std::ofstream output(src);
+    output << text;
+    return output.good();
+}
+
+bool FileManager::copyFile(const std::string& src, const std::string& dst) {
+    std::ifstream input(src, std::ios::binary);
+    std::ofstream output(dst, std::ios::binary);
+
+    output << input.rdbuf();
+
+    return output.good();
+}
+
+bool FileManager::combineFile(const std::string& first, 
+                              const std::string& second, 
+                              const std::string& dst) {
+    std::ofstream output(dst.c_str(), output.in | output.binary | output.ate);
+    std::ifstream input(second.c_str());
+    output << input.rdbuf();
+
+    return output.good();
+}
+                 
+bool FileManager::removeFile(const std::string& src) {
+     return (std::remove(src.c_str()) == 0);
 }
 
 void FileManager::create(const std::string& src, const std::string& text, bool overwrite) {
@@ -15,8 +49,9 @@ void FileManager::create(const std::string& src, const std::string& text, bool o
         throw std::invalid_argument(src + " already exists");
     }
 
-    std::ofstream output(src);
-    output << text;
+    if (!FileManager::retry(FileManager::createFile, kRetryCount, kRetryWaitMs, src, text)) {
+        throw std::invalid_argument("Create failed");
+    }
 }
 
 void FileManager::copy(const std::string& src, const std::string& dst, bool overwrite) {
@@ -28,10 +63,9 @@ void FileManager::copy(const std::string& src, const std::string& dst, bool over
         throw std::invalid_argument(dst + " already exists");
     }
 
-    std::ifstream input(src, std::ios::binary);
-    std::ofstream output(dst, std::ios::binary);
-
-    output << input.rdbuf();
+    if (!FileManager::retry(FileManager::copyFile, kRetryCount, kRetryWaitMs, src, dst)) {
+        throw std::invalid_argument("Create failed");
+    }
 }
 
 void FileManager::combine(const std::string& first, 
@@ -56,9 +90,9 @@ void FileManager::combine(const std::string& first,
 
     FileManager::copy(first, dst);
 
-    std::ofstream output(dst.c_str(), output.in | output.binary | output.ate);
-    std::ifstream input(second.c_str());
-    output << input.rdbuf();
+    if (!FileManager::retry(FileManager::combineFile, kRetryCount, kRetryWaitMs, first, second, dst)) {
+        throw std::invalid_argument("Create failed");
+    }
 }
 
 void FileManager::remove(const std::string& src) {
@@ -66,8 +100,8 @@ void FileManager::remove(const std::string& src) {
         return;
     }
 
-    if (std::remove(src.c_str()) != 0) {
-        throw std::runtime_error("Failed to delete " + src);
+    if (!FileManager::retry(FileManager::removeFile, kRetryCount, kRetryWaitMs, src)) {
+        throw std::invalid_argument("Create failed");
     }
 }
 
--- common/filemanager.h
+++ common/filemanager.h
@@ -1,12 +1,37 @@
 #pragma once
 
 #include <string>
+#include <thread>
+#include <chrono>
 
 namespace task {
 
 class FileManager final {
 private:
     static bool exists(const std::string& src);
+
+    static bool createFile(const std::string& src, const std::string& text);
+
+    static bool copyFile(const std::string& src, const std::string& dst);
+
+    static bool combineFile(const std::string& first, 
+                            const std::string& second, 
+                            const std::string& dst);
+                        
+    static bool removeFile(const std::string& src);
+
+    // Retry pattern in case if some files are locked
+    template<class Fn, typename... Ts>
+    static bool retry(Fn fn, int retryCount, int waitInMs, Ts... args) {
+        for (int i = 0; i < retryCount; ++i) {
+            if (fn(args ...)) {
+                return true;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(waitInMs));
+        }
+
+        return false;
+    }
 
 public:
     FileManager() = delete;
